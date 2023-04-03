2154200c69ea4d064427f34740ba29580ba50a9e
Darllington Gomes <darllingtongomes3@gmail.com>
2023-04-02 22:40:30 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode(){
+  const html = document.documentElement
+  html.classList.toggle("light")
+const img = document.querySelector("#profile img")
+
+  if(html.classList.contains('light')){
+img.setAttribute('src', './assets/profileLightCrop.png')
+img.setAttribute('alt', 'Darllington olhando para um mural de quadros')
+html.setAttribute('class', 'light')
+  } else{
+    html.setAttribute('class', 'dark');
+img.setAttribute('src', './assets/profileDarkCrop.png')
+img.setAttribute('alt', 'Darllington de blusa azul, olhando para a tela do celular')
+  }
+}
+
